00466848a29570b836b7afd4da023832c8fa6f0d
longnghia <41385034+longnghia@users.noreply.github.com>
2024-12-05 22:33:21 +0700

    Release 1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.3.0"
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
