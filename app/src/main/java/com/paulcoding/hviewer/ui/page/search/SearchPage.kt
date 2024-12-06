1965593408cb3f0b709ee96fb7666adf8329103f
longnghia <41385034+longnghia@users.noreply.github.com>
2024-12-06 10:31:32 +0700

    Update search box

--- app/src/main/java/com/paulcoding/hviewer/ui/page/search/SearchPage.kt
+++ app/src/main/java/com/paulcoding/hviewer/ui/page/search/SearchPage.kt
@@ -13,7 +13,8 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Edit
+import androidx.compose.material.icons.outlined.Clear
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -99,9 +100,16 @@ fun SearchPage(
                         imeAction = ImeAction.Go
                     ),
                     keyboardActions = KeyboardActions(onGo = { submit() }),
-                    modifier = Modifier.focusRequester(focusRequester)
+                    modifier = Modifier.focusRequester(focusRequester).weight(1f),
+                    trailingIcon = {
+                        if (query.isNotEmpty())
+                            HIcon(Icons.Outlined.Clear) {
+                                query = ""
+                                focusRequester.requestFocus()
+                            }
+                    }
                 )
-                HIcon(Icons.Outlined.Edit) { submit() }
+                HIcon(Icons.Outlined.Search) { submit() }
             }
             PageContent(appViewModel = appViewModel, viewModel = viewModel) { post ->
                 navToImages(post)
