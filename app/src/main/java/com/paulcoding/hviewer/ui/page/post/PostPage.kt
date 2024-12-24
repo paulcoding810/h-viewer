64b5ea29254c0cd584f573ce341267ff30d30edd
paulcoding810 <41385034+longnghia@users.noreply.github.com>
2024-12-24 13:49:00 +0700

    Show progress in post page

--- app/src/main/java/com/paulcoding/hviewer/ui/page/post/PostPage.kt
+++ app/src/main/java/com/paulcoding/hviewer/ui/page/post/PostPage.kt
@@ -5,6 +5,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -14,6 +15,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -109,7 +111,14 @@ fun PostPage(appViewModel: AppViewModel, goBack: () -> Unit) {
             enter = fadeInWithBlur(),
             exit = fadeOutWithBlur(),
         ) {
-            HBackIcon { goBack() }
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                HBackIcon { goBack() }
+                Text("${uiState.postPage}/${uiState.postTotalPage}")
+            }
         }
 
         if (selectedImage != null) {
