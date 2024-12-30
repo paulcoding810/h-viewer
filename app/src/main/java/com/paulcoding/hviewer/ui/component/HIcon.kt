4145d75373b6ec0e76f8310af3e7b193edde81b5
paulcoding810 <41385034+longnghia@users.noreply.github.com>
2024-12-30 17:25:36 +0700

    Fix favorite icon color

--- app/src/main/java/com/paulcoding/hviewer/ui/component/HIcon.kt
+++ app/src/main/java/com/paulcoding/hviewer/ui/component/HIcon.kt
@@ -1,6 +1,8 @@
 package com.paulcoding.hviewer.ui.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
@@ -11,6 +13,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
@@ -23,10 +26,22 @@ fun HBackIcon(onClick: () -> Unit) {
 }
 
 @Composable
-fun HFavoriteIcon(modifier: Modifier = Modifier, isFavorite: Boolean, onClick: () -> Unit) {
+fun HFavoriteIcon(
+    modifier: Modifier = Modifier,
+    isFavorite: Boolean,
+    rounded: Boolean = false,
+    onClick: () -> Unit
+) {
     val icon = if (isFavorite) Icons.Filled.Favorite else Icons.Filled.FavoriteBorder
-    val tint: Color = if (isFavorite) Color.Red else Color.Gray
-    HIcon(modifier = modifier, imageVector = icon, tint = tint, onClick = onClick)
+    val tint: Color = if (isFavorite) Color.Red else LocalContentColor.current
+
+    HIcon(
+        modifier = modifier,
+        imageVector = icon,
+        onClick = onClick,
+        tint = tint,
+        rounded = rounded
+    )
 }
 
 @Composable
@@ -36,9 +51,16 @@ fun HIcon(
     size: Int = 24,
     modifier: Modifier = Modifier,
     tint: Color = LocalContentColor.current,
+    rounded: Boolean = false,
     onClick: () -> Unit
 ) {
-    IconButton(onClick = { onClick() }, modifier = modifier) {
+    IconButton(
+        onClick = { onClick() },
+        modifier = if (rounded) modifier
+            .clip(CircleShape)
+            .background(Color.White)
+        else modifier
+    ) {
         Icon(imageVector, description, tint = tint, modifier = Modifier.size(size.dp))
     }
 }
