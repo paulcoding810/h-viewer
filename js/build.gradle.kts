12ec891db029b7b70882f29eb8abb50dfa8824d7
paulcoding810 <41385034+longnghia@users.noreply.github.com>
2025-01-20 20:55:24 +0700

    Publish to jitpack

--- js/build.gradle.kts
+++ js/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    `maven-publish`
 }
 
 android {
@@ -53,4 +54,39 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("release") {
+                from(components["release"])
+
+                groupId = "com.github.paulcoding810"
+                artifactId = "android-js"
+                version = "1.0.0"
+
+                pom {
+                    name.set("android-js")
+                    description.set("Rhino wrapper for android kotlin")
+                    url.set("https://github.com/paulcoding810/h-viewer")
+
+                    licenses {
+                        license {
+                            name.set("The Apache License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+
+                    developers {
+                        developer {
+                            id.set("paulcoding810")
+                            name.set("Paul Nguyen")
+                            email.set("paulcoding810@gmail.com")
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
