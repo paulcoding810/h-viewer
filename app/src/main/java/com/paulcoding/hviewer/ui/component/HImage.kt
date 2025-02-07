e3dfe68f43ff37526ee85a27928fc7d1cc54405b
paulcoding810 <41385034+longnghia@users.noreply.github.com>
2025-02-07 23:23:17 +0700

    fix cannot load some image without user-agent

--- app/src/main/java/com/paulcoding/hviewer/ui/component/HImage.kt
+++ app/src/main/java/com/paulcoding/hviewer/ui/component/HImage.kt
@@ -6,14 +6,23 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import coil3.compose.SubcomposeAsyncImage
+import coil3.network.NetworkHeaders
+import coil3.network.httpHeaders
 import coil3.request.ImageRequest
 import coil3.request.crossfade
 
 @Composable
 fun HImage(url: String, modifier: Modifier = Modifier) {
+    val headers = NetworkHeaders.Builder()
+        .set(
+            "User-Agent",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+        )
+        .build()
     SubcomposeAsyncImage(
         model = ImageRequest.Builder(LocalContext.current)
             .data(url)
+            .httpHeaders(headers)
             .crossfade(200)
             .build(),
         contentDescription = url,
