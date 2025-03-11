614c59f1a655eba9a669767ca99af75d4a752bbf
paulcoding810 <longnghia2.00@gmail.com>
2025-03-11 20:05:01 +0700

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 1
-        versionName = "1.4.0"
+        versionName = "1.4.1"
 
         buildConfigField("String", "REPO_URL", "\"$repoUrl\"")
 
