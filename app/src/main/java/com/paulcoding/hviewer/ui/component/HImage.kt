4b66b504e7883f30ba8e5e166748e0353da7dbcc
paulcoding810 <longnghia2.00@gmail.com>
2025-03-15 16:59:48 +0700

    Remove SubcomposeAsyncImage to improve performance

--- app/src/main/java/com/paulcoding/hviewer/ui/component/HImage.kt
+++ app/src/main/java/com/paulcoding/hviewer/ui/component/HImage.kt
@@ -5,29 +5,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import coil3.compose.SubcomposeAsyncImage
+import coil3.compose.AsyncImage
 import coil3.network.NetworkHeaders
 import coil3.network.httpHeaders
 import coil3.request.ImageRequest
-import coil3.request.crossfade
+
+private val customHeaders = NetworkHeaders.Builder()
+    .set(
+        "User-Agent",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+    )
+    .build()
 
 @Composable
 fun HImage(url: String, modifier: Modifier = Modifier) {
-    val headers = NetworkHeaders.Builder()
-        .set(
-            "User-Agent",
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
-        )
-        .build()
-    SubcomposeAsyncImage(
+    AsyncImage(
         model = ImageRequest.Builder(LocalContext.current)
             .data(url)
-            .httpHeaders(headers)
-            .crossfade(200)
+            .httpHeaders(customHeaders)
             .build(),
         contentDescription = url,
-        loading = { HLoading() },
-        error = { HEmpty() },
         modifier = modifier.fillMaxWidth(),
         contentScale = ContentScale.FillWidth,
     )
